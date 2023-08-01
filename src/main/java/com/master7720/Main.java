e73bc221c5001b889db79e7953b9625b9f6a0d83
Master7729 <thomasjameslei@gmail.com>
2023-08-01 12:57:57 -0700

    a little improvement

--- src/main/java/com/master7720/Main.java
+++ src/main/java/com/master7720/Main.java
@@ -3,46 +3,7 @@ package com.master7720;
 import com.master7720.decrypter.*;
 import com.master7720.encrypter.*;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.InvalidParameterSpecException;
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
-    private static final Map<String, Encrypter> ENCRYPTERS = new HashMap<>();
-    private static final Map<String, Decrypter> DECRYPTERS = new HashMap<>();
-
-    static {//has to be a better way of doing this
-        ENCRYPTERS.put(EncryptionType.BASE64, Base64Encrypter::encrypt);
-        ENCRYPTERS.put(EncryptionType.ROT13, ROT13Encrypter::encrypt);
-        ENCRYPTERS.put(EncryptionType.BASE16, Base16Encrypter::encrypt);
-        ENCRYPTERS.put(EncryptionType.BASE85, Base85Encrypter::encrypt);
-        ENCRYPTERS.put(EncryptionType.QUOTED_PRINTABLE, QuotedPrintableEncrypter::encrypt);
-        ENCRYPTERS.put(EncryptionType.ASCII85, Ascii85Encrypter::encrypt);
-        ENCRYPTERS.put(EncryptionType.Z85, Z85Encrypter::encrypt);
-        ENCRYPTERS.put(EncryptionType.VIGENERE, VigenereEncrypter::encrypt);
-        ENCRYPTERS.put(EncryptionType.AES, AESEncrypter::encrypt);
-        ENCRYPTERS.put(EncryptionType.RSA, RSAEncrypter::encrypt);
-        ENCRYPTERS.put(EncryptionType.DES, DESEncrypter::encrypt);
-
-        DECRYPTERS.put(EncryptionType.BASE64, Base64Decrypter::decrypt);
-        DECRYPTERS.put(EncryptionType.ROT13, ROT13Decrypter::decrypt);
-        DECRYPTERS.put(EncryptionType.BASE16, Base16Decrypter::decrypt);
-        DECRYPTERS.put(EncryptionType.BASE85, Base85Decrypter::decrypt);
-        DECRYPTERS.put(EncryptionType.QUOTED_PRINTABLE, QuotedPrintableDecrypter::decrypt);
-        DECRYPTERS.put(EncryptionType.ASCII85, Ascii85Decrypter::decrypt);
-        DECRYPTERS.put(EncryptionType.Z85, Z85Decrypter::decrypt);
-        DECRYPTERS.put(EncryptionType.VIGENERE, VigenereDecrypter::decrypt);
-        DECRYPTERS.put(EncryptionType.AES, AESDecrypter::decrypt);
-        DECRYPTERS.put(EncryptionType.RSA, RSADecrypter::decrypt);
-        DECRYPTERS.put(EncryptionType.DES, DESDecrypter::decrypt);
-    }
-
     public static void main(String[] args) throws Exception {
         if (args.length < 3) {
             displayUsage();
@@ -60,24 +21,12 @@ public class Main {
 
         switch (command) {
             case "encrypt":
-                if (ENCRYPTERS.containsKey(type)) {
-                    String encryptedText = ENCRYPTERS.get(type).encrypt(text);
-                    System.out.println("Encrypted: " + encryptedText);
-                } else {
-                    System.out.println("Invalid encryption type. Use 'base64', 'caesar', 'base16', 'base85', 'quoted_printable', 'ascii85', 'z85'.");
-                    displayUsage();
-                    System.exit(1);
-                }
+                String encryptedText = encrypt(type, text);
+                System.out.println("Encrypted: " + encryptedText);
                 break;
             case "decrypt":
-                if (DECRYPTERS.containsKey(type)) {
-                    String decryptedText = DECRYPTERS.get(type).decrypt(text);
-                    System.out.println("Decrypted: " + decryptedText);
-                } else {
-                    System.out.println("Invalid decryption type. Use 'base64', 'caesar', 'base16', 'base85', 'quoted_printable', 'ascii85', 'z85'.");
-                    displayUsage();
-                    System.exit(1);
-                }
+                String decryptedText = decrypt(type, text);
+                System.out.println("Decrypted: " + decryptedText);
                 break;
             default:
                 System.out.println("Invalid command. Use 'encrypt' or 'decrypt'.");
@@ -86,27 +35,77 @@ public class Main {
         }
     }
 
-    private interface Encrypter {
-        String encrypt(String text) throws Exception;
+    public static String encrypt(String type, String text) throws Exception {
+        switch (type) {
+            case EncryptionType.BASE64:
+                return Base64Encrypter.encrypt(text);
+            case EncryptionType.ROT13:
+                return ROT13Encrypter.encrypt(text);
+            case EncryptionType.BASE16:
+                return Base16Encrypter.encrypt(text);
+            case EncryptionType.BASE85:
+                return Base85Encrypter.encrypt(text);
+            case EncryptionType.QUOTED_PRINTABLE:
+                return QuotedPrintableEncrypter.encrypt(text);
+            case EncryptionType.ASCII85:
+                return Ascii85Encrypter.encrypt(text);
+            case EncryptionType.Z85:
+                return Z85Encrypter.encrypt(text);
+            case EncryptionType.AES:
+                return AESEncrypter.encrypt(text);
+            case EncryptionType.RSA:
+                return RSAEncrypter.encrypt(text);
+            case EncryptionType.DES:
+                return DESEncrypter.encrypt(text);
+            case EncryptionType.VIGENERE:
+                return VigenereEncrypter.encrypt(text, "SECRET_KEY");
+            default:
+                throw new IllegalArgumentException("Invalid decryption type. Use 'base64, rot13, base16, base85, quoted_printable, ascii85, z85, vigenere, aes, rsa, des");
+        }
     }
 
-    private interface Decrypter {
-        String decrypt(String encryptedText) throws Exception;
+    public static String decrypt(String type, String encryptedText) throws Exception {
+        switch (type) {
+            case EncryptionType.BASE64:
+                return Base64Decrypter.decrypt(encryptedText);
+            case EncryptionType.BASE16:
+                return Base16Decrypter.decrypt(encryptedText);
+            case EncryptionType.BASE85:
+                return Base85Decrypter.decrypt(encryptedText);
+            case EncryptionType.QUOTED_PRINTABLE:
+                return QuotedPrintableDecrypter.decrypt(encryptedText);
+            case EncryptionType.ASCII85:
+                return Ascii85Decrypter.decrypt(encryptedText);
+            case EncryptionType.Z85:
+                return Z85Decrypter.decrypt(encryptedText);
+            case EncryptionType.AES:
+                return AESDecrypter.decrypt(encryptedText);
+            case EncryptionType.RSA:
+                return RSADecrypter.decrypt(encryptedText);
+            case EncryptionType.DES:
+                return DESDecrypter.decrypt(encryptedText);
+            case EncryptionType.VIGENERE:
+                return VigenereDecrypter.decrypt(encryptedText, "SECRET_KEY");
+            default:
+                throw new IllegalArgumentException("Invalid decryption type. Use 'base64, rot13, base16, base85, quoted_printable, ascii85, z85, vigenere, aes, rsa, des");
+        }
     }
 
-    private static void displayUsage() {
-        System.out.println("Usage: java -jar EncryptionDecryption.jar <command> <type> <text>");
+    public static void displayUsage() {
+        System.out.println("Usage: java -jar <jar-file-name>.jar <command> <type> <text>");
         System.out.println("Commands: encrypt, decrypt, help");
-        System.out.println("Types: base64, caesar, base16, base85, quoted_printable, ascii85, z85");
+        System.out.println("Types: base64, rot13, base16, base85, quoted_printable, ascii85, z85, vigenere, aes, rsa, des");
     }
-    private static void displayHelp() {
+
+    public static void displayHelp() {
         displayUsage();
         System.out.println("\nDescription:");
         System.out.println("The EncryptionDecryption program allows you to encrypt or decrypt text using different encryption types.");
         System.out.println("\nExamples:");
-        System.out.println("java -jar JavaEncryptionDecryption.jar encrypt base64 'Hello, World!'");
-        System.out.println("java -jar JavaEncryptionDecryption.jar decrypt base64 'Khoor, Zruog!'");
+        System.out.println("java -jar <jar-file-name>.jar encrypt base64 'Hello, World!'");
+        System.out.println("java -jar <jar-file-name>.jar decrypt base64 'Khoor, Zruog!'");
     }
+
     private static class EncryptionType {
         public static final String BASE64 = "base64";
         public static final String BASE16 = "base16";
